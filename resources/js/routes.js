a43fe1f17893c2fbdbb847fdd28a76b0c1b1634c
Ohrimus148 <Oleg.Okhrymenko@gmail.com>
2018-12-05 01:58:14 +0200

    Root Vue Components, Router

---
+++ resources/js/routes.js
@@ -0,0 +1,54 @@
+import Home from './components/Home.vue';
+import Login from './components/auth/Login.vue';
+import CustomersMain from './components/customers/Main.vue';
+import CustomersList from './components/customers/List.vue';
+import NewCustomer from './components/customers/New.vue';
+import CsvCustomers from './components/customers/Csv.vue';
+// import EditCustomer from './components/customers/Edit.vue';
+import Customer from './components/customers/View.vue';
+
+export const routes = [
+    {
+        path: '/',
+        component: Home,
+        meta: {
+            requiresAuth: true
+        }
+    },
+    {
+        path: '/login',
+        component: Login
+    },
+    {
+        path: '/customers',
+        component: CustomersMain,
+        meta: {
+            requiresAuth: true
+        },
+        children: [
+            {
+                path: '/',
+                component: CustomersList
+            },
+            {
+                path: 'new',
+                component: NewCustomer
+            },
+            {
+                path: 'csv',
+                component: CsvCustomers
+            },
+            {
+                path: ':id',
+                component: Customer
+            },
+
+            // {
+            //     path: 'edit/:id',
+            //     component: EditCustomer
+            // },
+
+
+        ]
+    }
+];
